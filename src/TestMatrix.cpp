483ed3b89c59e6f499b8e5e4fe8c2b336b041b82
va3fod <valicristea@outlook.com>
2023-01-10 15:08:33 -0500

    minor

--- src/TestMatrix.cpp
+++ src/TestMatrix.cpp
@@ -6,6 +6,10 @@ int main()
 {
     // sample use of Matrix class
     Matrix AA(3, 3, 1);
+	Matrix BB = AA;
+	Matrix bb(3, 1, 1);
+	Matrix brez(3, 1, 1);
+	Matrix xx;
 
 	AA[0][0] = 4;
 	AA[0][1] = 1;
@@ -18,36 +22,30 @@ int main()
 	AA[2][0] = 1;
 	AA[2][1] = 2;
     AA[2][2] = 3;
-	std::cout << "AA = " << std::endl;
-	std::cout << AA << std::endl;
-	
-
-	Matrix bb(3, 1, 1);    
-	Matrix brez(3, 1, 1);
 	
 	bb[0][0] = 2;
 	bb[1][0] = 1;
 	bb[2][0] = 1;
-
-	brez = 2 - bb;
-	std::cout << "brez = \n" <<brez<< std::endl;
+	bb.print("bb");
 	
-	brez = bb - 2 ;
-	std::cout << "brez = \n" << brez << std::endl;
+	brez = AA*bb;
+	brez.print("brez");
 	
+	brez = (AA*bb)  + (bb^bb) ;
+	brez.print("(AA*bb)  + (bb^bb)");
+	//std::cout << "brez = \n" << brez << std::endl;
 	
-	//bb.print("bb");
-    
-	//Matrix xx(3, 1, -99); // default to -99  just for testing
+	BB = ~AA;
 
-	//xx = conjugateGradient(AA, bb);
-
-	//xx = AA.inv() * bb;
-    
-	//xx.print("xx");
+	AA.print("AA");
+	std::cout << "BB = \n" << BB << std::endl;
+	
+	// Ax=b;
+	xx = AA.inv() * bb;
+	xx.print("xx = A.inv() * b");
 
     // print the inverse of AA
-	//AA.inv().print("inverse of AA");
+	AA.inv().print("inverse of AA");
 	
 
     std::cin.get();
